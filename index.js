5ab1e8bcbaf980c0ebc990b39b992414d9252007
Punam <punamkumari399@gmail.com>
2024-09-03 14:10:23 +0530

    url modified

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 //   "https://chat-dash-gamma.vercel.app/",
 //   // "http://localhost:5173",
 // ];
-const allowedOrigins = "https://chat-dash-gamma.vercel.app/";
+const allowedOrigins = "https://chat-dash-gamma.vercel.app";
 
 app.use(
   cors({
